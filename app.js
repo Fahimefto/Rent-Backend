be21e90847806760c6e93fd63590a64cc130656e
Fahimefto <ifto.141.fi@gmail.com>
2022-11-16 13:53:55 +0600

    origin fixed for depploy

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const upload = require("./routers/upload");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({ origin: "https://rent-fahimefto.vercel.app", credentials: true })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   fileUpload({
